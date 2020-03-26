afdfa751b1760d970b8b875f75c3e5b95fa601f9
Stoyan Stoyanov <stoyan@lionheartapplications.com>
2020-03-26 16:52:46 +0200

    git added the sample code

--- TestUIReplicantView/ViewController.swift
+++ TestUIReplicantView/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
+
+// MARK: - Class Definition
+
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let image = view.snapshot
+        
+        // next line crashes with:
+        // *** Terminating app due to uncaught exception 'NSInvalidArgumentException',
+        // reason: '-[_UIReplicantView _isSymbolImage]: unrecognized selector sent to instance 0x115907c00'
+        imageView.image = image
     }
-
-
 }
 
+// MARK: - Helpers
+
+extension UIView {
+    
+    func snapshot(at scale: CGFloat) -> UIImage? {
+        
+        let renderer = UIGraphicsImageRenderer(size: bounds.size)
+        let image = renderer.image { [weak self] context in
+            self?.drawHierarchy(in: self?.bounds ?? .zero, afterScreenUpdates: true)
+        }
+        
+        return image
+    }
+    
+    func snapshotOld(at scale: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, scale)
+        guard let currentContext = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: currentContext)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    @objc var snapshot: UIImage? {
+        snapshot(at: 3.0)
+//        snapshotOld(at: 3.0)
+    }
+}
