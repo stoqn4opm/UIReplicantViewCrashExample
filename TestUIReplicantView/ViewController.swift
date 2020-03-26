83fee1de8dd68d33cdd97c552ee31613e8e0e82c
Stoyan Stoyanov <stoyan@lionheartapplications.com>
2020-03-26 16:21:19 +0200

    Initial Commit

---
+++ TestUIReplicantView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestUIReplicantView
+//
+//  Created by Stoyan Stoyanov on 26/03/2020.
+//  Copyright © 2020 Stoyan Stoyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
